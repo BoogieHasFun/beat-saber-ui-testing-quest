94e32110eed381332dbee3710e6deadbfef19d79
boogie <76754631+BoogieHasFun@users.noreply.github.com>
2022-10-20 21:33:25 -0400

    :(

---
+++ README.md
@@ -0,0 +1,21 @@
+# boogietest
+
+testing
+this brings me stress
+please help me with this
+no one wants to help me
+maybe i should scrap it
+maybe i should just give up my goals.
+and live knowing i could've done something i wanted to do but i didn't
+i have strong belief that nobody cares and nobody wants to help, rather they want me to go away
+i don't know anymore
+
+### please help me finish this, if you find this
+
+-- boogie
+
+## Credits
+
+* [zoller27osu](https://github.com/zoller27osu), [Sc2ad](https://github.com/Sc2ad) and [jakibaki](https://github.com/jakibaki) - [beatsaber-hook](https://github.com/sc2ad/beatsaber-hook)
+* [raftario](https://github.com/raftario)
+* [Lauriethefish](https://github.com/Lauriethefish), [danrouse](https://github.com/danrouse) and [Bobby Shmurner](https://github.com/BobbyShmurner) for [this template](https://github.com/Lauriethefish/quest-mod-template)
---
+++ src/hook.hpp
@@ -0,0 +1,36 @@
+// Think of these as C#, using MainMenuViewController, using UnityEngine.UI.Button, using HMUI.CurvedTextMeshPro ect.
+// Classes without a namespace are assigned to the GlobalNamespace
+
+// why wont this work
+#include "GlobalNamespace/MainMenuViewController.hpp"
+#include "UnityEngine/UI/Button.hpp"
+#include "UnityEngine/GameObject.hpp"
+#include "HMUI/CurvedTextMeshPro.hpp"
+
+// Create a hook struct, named MainMenuUIHook.
+// Target "void MainMenuViewController::DidActivate" and takes the following arguments:
+// bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling
+
+// General format: MAKE_HOOK_MATCH(HookName, method, method return type, method class pointer, arguments...) { 
+//  HookName(arguments...);
+//  // your code here 
+//}
+
+// it was working eariler 
+MAKE_HOOK_MATCH(MainMenuUIHook, &GlobalNamespace::MainMenuViewController::DidActivate, void, GlobalNamespace::MainMenuViewController
+*self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+    // Run the original method before our code.
+    // Note, you can run the original method after our code if you want to change arguments.
+    MainMenuUIHook(self, firstActivation, addedToHierarchy, screenSystemEnabling); 
+    
+    
+    // Get the _soloButton text using the dyn_ method and simple unity jazz. dyn_ safely get fields and shouldn't change
+    // much during updates.
+    
+    UnityEngine::UI::Button *soloMenuButton = self->dyn__soloButton();
+    UnityEngine::GameObject *gameObject = soloMenuButton->get_gameObject();
+    HMUI::CurvedTextMeshPro *soloMenuText = gameObject->GetComponentInChildren<HMUI::CurvedTextMeshPro *>();
+    
+    // Set the text to "Skill Issue"
+    soloMenuText->SetText("Skill Issue");
+}
